97ba48a90d719d08d608461f8a63c5cec1b01f0f
jay77 <jayakrishnasurampudi77@gmail.com>
2021-08-20 23:32:06 +0530

    initial commit for angular-springboot application

---
+++ OneDrive/Desktop/workspace-spring-tool-suite-4-4.11.0.RELEASE/angular-frontend/src/app/userserve.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+// injectable decorator will make typescript class behaves like angular service 
+export class UserserveService { 
+private baseUrl = "http://localhost:8085/api"; 
+constructor(private http: HttpClient) { }
+
+// to send form data from service to springboot 
+getdata():Observable<User[]>
+{
+  return this.http.get<User[]>(`${this.baseUrl}/user`);
+    }
+
+    
+save(data:any):Observable<any>
+{
+  return this.http.post<any>(`${this.baseUrl}/user`,data)
+  console.log(data);
+
+}
+
+deleteuser(id:any):Observable<any>
+{
+  return this.http.delete<any>(`${this.baseUrl}/user/${id}`);
+    }
+
+        
+updateuser(data:any,id:any):Observable<any>
+{
+  return this.http.put<any>(`${this.baseUrl}/user/${id}`,data)
+  console.log(data);
+
+}
+
+
+
+}
+
+
+
